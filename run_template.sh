b55e4cb538565f455314cbf777326d9cf096f3ad
yhcchaos <yhcchaos@gmail.com>
2023-12-19 09:47:03 +0000

    run_template.sh: update train and test command

--- run_template.sh
+++ run_template.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 mode=$1
-total_steps=50000000
-base_logdir="logs/e12m2_1219_1-0.02*DeltaDivSumDelta*f-2_steps5000wan_f1-3-3_b12-192-exp5_q1-16-exp5bdp_l0-0.1-exp5_d1-85-7_rt30_a10"
+total_steps=
+base_logdir=
 mkdir $base_logdir
-checkpoint=""
+checkpoint=
 checkpoint_path=$base_logdir/checkpoint/$checkpoint.tar
 echo base_logdir=$base_logdir
 echo checkpoint=$checkpoint_path
 echo total_steps=$total_steps
 if [ $mode == "train" ]; then
+	cp train/experiments.yml $base_logdir/experiments.yml
 	GLOG_minloglevel=0 nohup python3 -m train.train --num_actions 11 --cc_env_history_size=20 --cc_env_reward_throughput_factor 1. --cc_env_reward_delay_factor 0.02 --cc_env_reward_packet_loss_factor 2.  --observation_length 48 --num_actors=10 --total_steps=$total_steps --base_logdir=$base_logdir  --mode=train >$base_logdir/ss_train.log 2>&1 &
 else
+	cp train/experiments_test.yml $base_logdir/experiments_test.yml
 	GLOG_minloglevel=0 nohup python3 -m train.train --num_actions 11 --cc_env_history_size=20 --cc_env_reward_throughput_factor 1. --cc_env_reward_delay_factor 0.02 --cc_env_reward_packet_loss_factor 2.  --observation_length 48 --num_actors=10 --total_steps=5000000 --base_logdir=$base_logdir  --checkpoint=$checkpoint_path --mode=test --do_log >$base_logdir/ss_test.log 2>&1 &
 fi
