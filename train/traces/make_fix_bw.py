3c32c68958b3c3289cb701ddb8444c5b5bc64c10
yhchaos <yhcchaos@gmail.com>
2023-12-31 13:33:08 +0000

    add some traces

--- train/traces/make_fix_bw.py
+++ train/traces/make_fix_bw.py
@@ -1,10 +1,9 @@
 import os
 import sys
-bandwidths = [36, 60, 72, 84, 120, 144, 168, 240, 288, 336]
+bandwidths = [12,24,36,48,60,72,84,96,108,120,132,144,156,168,180,192]
 for bw in bandwidths:
     with open(os.path.join("fix", "{}mbps.trace".format(bw)), 'w') as f:
         f.write("#"+str(1)+" "+str(bw)+"\n")
         send_packets = bw // 12
         for j in range(send_packets):
             f.write(str(1)+"\n")
-                
